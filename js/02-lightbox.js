d8d0257ad47f76d772a897ec1a39fbef1566df9f
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-11 19:09:52 +0200

    Update 02-lightbox.js
    add galleryList

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryImageList = createGalleryList(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryImageList);
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join('');
+}
