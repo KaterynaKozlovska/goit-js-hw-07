d6f0efb9855d92b207b2e57e17b49fa0342b1ea8
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-02-11 17:47:34 +0200

    Update 01-gallery.js
    add gallaryImageList

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryImageList = createGalleryList(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryImageList);
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+        <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+        />
+       </a>
+      </div>
+`;
+    })
+    .join('');
+}
